b9c021a4f068a3980c32f19f31dc39ff1af8cf0f
Abraão Silva <abraao.reis.reis@gmail.com>
2023-06-23 14:01:00 -0300

    feat: adicionado página Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Sidebar from "../components/Sidebar";
+import HomeContent from "../components/HomeContent";
+
+const Home = () => {
+  return (
+    <>
+      <Sidebar />
+      <HomeContent />
+    </>
+  );
+};
+
+export default Home;
