ae68dd0f3432d6568cd783b17477ee6c2a099333
Abraão Silva <abraao.reis.reis@gmail.com>
2023-06-23 14:07:33 -0300

    fix: salvando alterações pendentes antes do rebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import { Fragment } from "react";
+import GlobalStyle from "./styles/globalStyles";
+import Home from "./pages/Home";
 
 const App = () => {
-  return <Fragment></Fragment>;
+  return (
+    <Fragment>
+      <GlobalStyle />
+      <Home />
+    </Fragment>
+  );
 };
 
 export default App;
